1f8c9f82fa01ee32980751918c481b8482ba56f2
pandeykunal57 <148645697+pandeykunal57@users.noreply.github.com>
2024-04-16 18:19:56 +0530

    Version 2

--- app/src/main/java/com/example/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.recordkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.commit
 import com.example.recordkeeper.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+@Suppress("DEPRECATION")
+ class MainActivity : AppCompatActivity(), View.OnClickListener,
+    BottomNavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
 
 
@@ -15,11 +21,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonCycling.setOnClickListener { onCyclingclicked() }
-        binding.buttonRunning.setOnClickListener { onRunningclicked() }
+
+        binding.bottomNav.setOnNavigationItemSelectedListener(this)
+
+
+        /* ALTERNATIVE
+        binding.bottomNav.setOnNavigationItemSelectedListener(object:BottomNavigationView.OnNavigationItemSelectedListener{
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        }) */
 
 
     }
+
 
     private fun onCyclingclicked() {
         supportFragmentManager.commit {
@@ -33,4 +49,22 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId==R.id.nav_cycling) {
+            onCyclingclicked()
+            return true
+        }
+        else if (item.itemId==R.id.nav_running){
+            onRunningclicked()
+            return true // true is returned that yes highlight the button in navigation pane when it si selected
+            } else {
+                return false
+        }
+        }
+
+    override fun onClick(p0: View?) {
+
     }
+
+}
